1a176d7bfa2d7d35d510410f612af8e4e4de2122
onurqnel <onur.onel2230@gmail.com>
2025-01-17 20:53:47 -0500

    Update .gitignore and clean untracked files

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -1,15 +1,22 @@
 #include <string>
 
-namespace log_line {
-    std::string message(std::string line) {
-        // return the message
+namespace log_line
+{
+    std::string message(std::string line)
+    {
+        int messageIndex = line.find(":");
+        return line.substr(messageIndex + 1);
     }
 
-    std::string log_level(std::string line) {
-        // return the log level
+    std::string log_level(std::string line)
+    {
+        int levelStartIndex = line.find("[");
+        int levelEndIndex = line.find("]");
+        return line.substr(levelStartIndex + 1, levelEndIndex - 1);
     }
 
-    std::string reformat(std::string line) {
-        // return the reformatted message
+    std::string reformat(std::string line)
+    {
+        return log_line::message, " (", log_line::log_level, ")";
     }
 }
